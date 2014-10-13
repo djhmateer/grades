4c2f1b14c6082cb512e8e2d4207920366a435cae
Dave Mateer <davemateer@gmail.com>
2014-10-13 17:05:54 +0100

    first commit

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grades {
+    public delegate void NameChangedDelegate(string oldValue, string newValue);
+    public class GradeBook {
+        public GradeBook(string name = "There is no name") {
+            Name = name;
+            grades = new List<float>();
+        }
+
+        public void AddGrade(float grade) {
+            if (grade >= 0 && grade < 100) {
+                grades.Add(grade);
+            }
+        }
+
+        public GradeStatistics ComputeStatistics() {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0f;
+            foreach (float grade in grades) {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+
+        private string _name;
+
+        public string Name {
+            get {
+                return _name;
+            }
+            set {
+                if (_name != value) {
+                    var oldValue = _name;
+                    _name = value;
+                    // Announce to world that something has changed
+                    if (NameChanged != null) {
+                        // It has no idea what will be called, only that the signature will be string oldValue, string newValue
+                        NameChanged(oldValue, value);
+                    }
+                }
+            }
+        }
+
+        // Field of type NameChangedDelegate
+        public NameChangedDelegate NameChanged;
+
+        //if (_name != value) {
+        //    var oldValue = _name;
+        //    _name = value;
+        //    if (NameChanged != null) {
+        //        NameChangedEventArgs args = new NameChangedEventArgs();
+        //        args.OldValue = oldValue;
+        //        args.NewValue = value;
+        //        NameChanged(this, args);
+        //    }
+        //}
+
+
+        //public event NamedChangedDelegate NameChanged;
+
+        private List<float> grades;
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Grades {
+    class Program {
+        static void Main(string[] args) {
+            GradeBook book = new GradeBook("Scott's Book");
+            book.AddGrade(91f);
+            book.AddGrade(89.1f);
+            book.AddGrade(75f);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            // Hooking up the delegate to point to another method
+            // Setting the public Field (of type NameChangedDelegate) to a new delegate and passing the method
+            // however anyone else can overwrite this
+            //book.NameChanged = new NameChangedDelegate(OnNameChanged);
+
+            // multicast delegate
+            book.NameChanged += OnNameChanged;
+            book.NameChanged += OnNameChanged;
+            book.NameChanged += OnNameChanged2;
+            // ahh would wipe out the previous ones
+            book.NameChanged = new NameChangedDelegate(OnNameChanged2);
+
+            book.Name = "Allen's Book";
+            WriteNames(book.Name);
+
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.HighestGrade);
+        }
+
+        private static void OnNameChanged2(string oldvalue, string newvalue) {
+            Console.WriteLine("***");
+        }
+
+        // This method will execute when a 
+        private static void OnNameChanged(string oldvalue, string newvalue) {
+            Console.WriteLine("Name changed from {0} to {1}", oldvalue, newvalue);
+        }
+
+        private static void WriteBytes(int value) {
+            byte[] bytes = BitConverter.GetBytes(value);
+            WriteByteArray(bytes);
+        }
+
+
+        private static void WriteBytes(float value) {
+            byte[] bytes = BitConverter.GetBytes(value);
+            WriteByteArray(bytes);
+        }
+
+        private static void WriteByteArray(byte[] bytes) {
+            foreach (byte b in bytes) {
+                Console.Write("0x{0:X} ", b);
+            }
+            Console.WriteLine();
+        }
+
+        private static void WriteNames(params string[] names) {
+            foreach (string name in names) {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
